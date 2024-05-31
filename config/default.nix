6892483e6fc44edd470e231a7d84f2c2482721a3
AJ Fagan <ajfagan@wisc.edu>
2024-05-31 05:21:06 -0500

    ading CHAD tree

--- config/default.nix
+++ config/default.nix
@@ -6,69 +6,71 @@ imports = [ ./bufferline.nix ];
 
 config = {
 
-	globals.mapleader = " ";
+  globals.mapleader = " ";
 
   plugins = {
-	lualine.enable = true;
+    lualine.enable = true;
   	
-	telescope.enable = true;
+    telescope.enable = true;
 
-	oil.enable = true;
+    oil.enable = true;
 
-	treesitter.enable = true;
+    treesitter.enable = true;
 
-	luasnip.enable = true;
+    luasnip.enable = true;
 
+    chadtree.enable = true;
 
-	  lsp = {
-		enable = true;
-		servers = {
-			lua-ls = {
-				enable = true;
-				settings.telemetry.enable = false;
-			};
-
-			rust-analyzer = {
-				enable = true;
-				installCargo = true;
-				installRustc = true;
-			};
-		};
-	  };
-
-  cmp.settings = {
-	enable = true;
-	autoEnableSources = true;
-	sources = [
-		{name = "nvim_lsp";}
-		{name = "path";}
-		{name = "buffer";}
-		{name = "luasnip";}
-	];
-	mapping = {
-		"<CR>" = "cmp.mapping.confirm({ select = true })";
-		"<Tab>" = {
-		  action = ''
-		    function(fallback)
-		      if cmp.visible() then
-			cmp.select_next_item()
-		      elseif luasnip.expandable() then
-			luasnip.expand()
-		      elseif luasnip.expand_or_jumpable() then
-			luasnip.expand_or_jump()
-		      elseif check_backspace() then
-			fallback()
-		      else
-			fallback()
-		      end
-		    end
-		  '';
-		  modes = [ "i" "s" ];
-		};
-	      };
+    lsp = {
+      enable = true;
+      servers = {
+        lua-ls = {
+	  enable = true;
+	  settings.telemetry.enable = false;
 	};
 
+
+	rust-analyzer = {
+	  enable = true;
+	  installCargo = true;
+	  installRustc = true;
+	};
+      };
+    };
+
+  cmp.settings = {
+    enable = true;
+    autoEnableSources = true;
+    sources = [
+      {name = "nvim_lsp";}
+      {name = "path";}
+      {name = "buffer";}
+      {name = "luasnip";}
+    ];
+    mapping = {
+      "<CR>" = "cmp.mapping.confirm({ select = true })";
+      "<Tab>" = {
+	action = ''
+	  function(fallback)
+	    if cmp.visible() then
+	      cmp.select_next_item()
+	      elseif luasnip.expandable() then
+		luasnip.expand()
+	      elseif luasnip.expand_or_jumpable() then
+		luasnip.expand_or_jump()
+	      elseif check_backspace() then
+		fallback()
+	      else
+		fallback()
+	      end 
+	    end
+	  '';
+	modes = [ "i" "s" ];
+      };
+    };
   };
+
+};
 
 
 
@@ -91,6 +93,10 @@ config = {
 	{
 		action = "<cmd>Telescope live_grep<CR>";
 		key = "<leader>g";
+	}
+	{
+    action = "<cmd>CHADopen<CR>";
+    key = "<leader>v";
 	}
   ];
 
